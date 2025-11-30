0ed2bad659ec2db98e8d2e5106aaeec82cab12dd
Ingenioz IT <contact@ingenioz.it>
2025-11-30 23:30:47 +0100

    implement stream seek

--- src/Http/Message/Stream.php
+++ src/Http/Message/Stream.php
@@ -5,16 +5,19 @@ declare(strict_types=1);
 namespace IngeniozIt\Psr\Http\Message;
 
 use BadMethodCallException;
+use IngeniozIt\Psr\Http\Message\Exception\CannotSeekStream;
 use IngeniozIt\Psr\Http\Message\Exception\CannotTellStream;
 use IngeniozIt\Psr\Http\Message\Exception\InvalidResource;
 use Psr\Http\Message\StreamInterface;
 
 use function feof;
 use function fclose;
+use function fseek;
 use function fstat;
 use function ftell;
 use function is_array;
 use function is_resource;
+use function stream_get_meta_data;
 
 class Stream implements StreamInterface
 {
@@ -84,13 +87,22 @@ class Stream implements StreamInterface
 
     public function isSeekable(): bool
     {
-        throw new BadMethodCallException('Not implemented');
+        if (!is_resource($this->resource)) {
+            return false;
+        }
+
+        $metadata = stream_get_meta_data($this->resource);
+        return $metadata['seekable'];
     }
 
-    /** @SuppressWarnings("PHPMD.UnusedFormalParameter") */
     public function seek(int $offset, int $whence = SEEK_SET): void
     {
-        throw new BadMethodCallException('Not implemented');
+        if (!$this->isSeekable()) {
+            throw new CannotSeekStream();
+        }
+
+        /** @phpstan-ignore argument.type */
+        fseek($this->resource, $offset, $whence);
     }
 
     public function rewind(): void
--- tests/Http/Message/StreamTest.php
+++ tests/Http/Message/StreamTest.php
@@ -6,6 +6,7 @@ namespace IngeniozIt\Psr\Tests\Http\Message;
 
 use IngeniozIt\Psr\Http\Message\Exception\InvalidResource;
 use IngeniozIt\Psr\Http\Message\Exception\CannotTellStream;
+use IngeniozIt\Psr\Http\Message\Exception\CannotSeekStream;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\StreamInterface;
@@ -161,6 +162,101 @@ class StreamTest extends TestCase
         return [
             'start position' => [$startPosition, false],
             'end position' => [$endPosition, true],
+        ];
+    }
+
+    /** @param StreamInterface $stream */
+    #[DataProvider('provideResourcesWithSeekable')]
+    public function testCanTellIfStreamIsSeekable(StreamInterface $stream, bool $expectedSeekable): void
+    {
+        $seekable = $stream->isSeekable();
+
+        self::assertEquals($expectedSeekable, $seekable);
+    }
+
+    /** @return array<string, array{StreamInterface, bool}> */
+    public static function provideResourcesWithSeekable(): array
+    {
+        /** @var resource $seekableResource */
+        $seekableResource = fopen('php://temp', 'r+');
+        $seekableStream = new Stream($seekableResource);
+
+        /** @var resource $nonSeekableResource */
+        $nonSeekableResource = fopen('php://stdin', 'r');
+        $nonSeekableStream = new Stream($nonSeekableResource);
+
+        /** @var resource $detachedResource */
+        $detachedResource = fopen('php://temp', 'r+');
+        $detachedStream = new Stream($detachedResource);
+        $detachedStream->detach();
+
+        return [
+                'seekable resource' => [$seekableStream, true],
+                'non-seekable resource' => [$nonSeekableStream, false],
+                'detached stream' => [$detachedStream, false],
+        ];
+    }
+
+    /** @param StreamInterface $stream */
+    #[DataProvider('provideSeekOperations')]
+    public function testCanSeek(StreamInterface $stream, int $offset, int $whence, int $expectedPosition): void
+    {
+        $stream->seek($offset, $whence);
+        $position = $stream->tell();
+
+        self::assertEquals($expectedPosition, $position);
+    }
+
+    /** @return array<string, array{StreamInterface, int, int, int}> */
+    public static function provideSeekOperations(): array
+    {
+        /** @var resource $resource1 */
+        $resource1 = fopen('php://temp', 'r+');
+        fwrite($resource1, 'foobar');
+        $stream1 = new Stream($resource1);
+
+        /** @var resource $resource2 */
+        $resource2 = fopen('php://temp', 'r+');
+        fwrite($resource2, 'foobar');
+        fseek($resource2, 2);
+        $stream2 = new Stream($resource2);
+
+        /** @var resource $resource3 */
+        $resource3 = fopen('php://temp', 'r+');
+        fwrite($resource3, 'foobar');
+        $stream3 = new Stream($resource3);
+
+        return [
+            'absolute position' => [$stream1, 3, SEEK_SET, 3],
+            'relative to current position' => [$stream2, 2, SEEK_CUR, 4],
+            'relative to end' => [$stream3, -2, SEEK_END, 4],
+        ];
+    }
+
+    /** @param StreamInterface $stream */
+    #[DataProvider('provideSeekExceptionCases')]
+    public function testThrowsExceptionWhenSeeking(StreamInterface $stream): void
+    {
+        self::expectException(CannotSeekStream::class);
+        self::expectExceptionMessage("Could not seek stream");
+        $stream->seek(0);
+    }
+
+    /** @return array<string, array{StreamInterface}> */
+    public static function provideSeekExceptionCases(): array
+    {
+        /** @var resource $nonSeekableResource */
+        $nonSeekableResource = fopen('php://stdin', 'r');
+        $nonSeekableStream = new Stream($nonSeekableResource);
+
+        /** @var resource $detachedResource */
+        $detachedResource = fopen('php://temp', 'r+');
+        $detachedStream = new Stream($detachedResource);
+        $detachedStream->detach();
+
+        return [
+            'non-seekable stream' => [$nonSeekableStream],
+            'detached stream' => [$detachedStream],
         ];
     }
 }
